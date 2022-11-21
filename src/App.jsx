27d2ba076e6d648be44cc745f57972eceacfb610
Ruth Aja <ruthajalah1@gmail.com>
2022-11-21 14:57:15 +0700

    delete comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import PlaceContentCenter from './components/PlaceContentCenter'
-import Todo from './components/Todo'
-import { useState } from 'react'
 import Card from './components/Card'
 import Button from './components/Button'
 import { useRef } from 'react'
 import Input from './components/Input'
 
 export default function App() {
-    // const [tick, setTick] = useState(0)
     const inputRef = useRef(null)
 
     function handleClick() {
         inputRef.current.focus()
-        // console.log(tick)
-        // tick.current = tick.current + 1
-        // const nextTick = tick + 1
-        // setTick(nextTick)
-        // console.log(nextTick)
-        // console.log(tick.current);
     }
 
     return (
         <PlaceContentCenter>
-            {/* <Todo /> */}
             <Card>
                 <Card.Tittle>useRef Hook</Card.Tittle>
                 <Card.Body>
@@ -30,7 +20,6 @@ export default function App() {
                     <Input placeholder={'Password'} className={'border border-slate-400'}></Input>
                     <Button onClick={handleClick}>Tick</Button>
                 </Card.Body>
-                {/* <Card.Footer>You clicked {tick.current} times</Card.Footer> */}
             </Card>
         </PlaceContentCenter>
     )
