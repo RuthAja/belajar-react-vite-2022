ea265c32d1f66e27d8d120c7c579ebcef341554d
Ruth Aja <ruthajalah1@gmail.com>
2022-11-21 15:50:02 +0700

    learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import PlaceContentCenter from './components/PlaceContentCenter'
 import { useEffect, useState } from 'react'
 import Input from './components/Input'
+import Button from './components/Button'
 
 export default function App() {
     const [name, setName] = useState('')
+    const [online, setOnline] = useState(false)
+    const [scrollPosition, setScrollPosition] = useState(window.scroll)
 
     // selalu di render setiap ada perubahan
     useEffect(() => {
@@ -13,11 +16,28 @@ export default function App() {
     // di render atau di panggil hanya di saat pertama saja
     useEffect(() => {
         console.log('first render')
-    },[])
+    }, [])
+
+    //ada perubahan jika di Trigger/ Picu
+    useEffect(() => {
+        console.log(`I am now ${online ? 'online' : 'offline'}`)
+    }, [online])
+
+    function updateScrollPosition() {
+        const windowScrolling = window.scrollY
+        console.log(`window scroll position: ${windowScrolling}`)
+        setScrollPosition(windowScrolling)
+    }
+    // componenWillUnmount | apa yang akan dilakuakn kalau mau di render
+    useEffect(() => {
+        window.addEventListener('scroll', updateScrollPosition)
+        // return () => {}
+    })
 
     return (
-        <PlaceContentCenter>
+        <div className={'h-[4000px]'}>
             <Input value={name} onChange={(e) => setName(e.target.value)}></Input>
-        </PlaceContentCenter>
+            <Button onClick={() => setOnline((online) => !online)}> Set Online</Button>
+        </div>
     )
 }
