93f9b3169a7e149563fc7fad08ec87382e8ff902
Ruth Aja <ruthajalah1@gmail.com>
2022-11-22 09:28:15 +0700

    learn AJAX & API using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import PlaceContentCenter from './components/PlaceContentCenter'
+import Card from './components/Card'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function App() {
     const [users, setUsers] = useState([])
 
     useEffect(() => {
         async function getUsers() {
-            const response = await fetch('http://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
-            console.log(data)
+            try {
+            const { data } = await axios('http://jsonplaceholder.typicode.com/users')
+            setUsers(data)
+            } catch (error) {
+                console.log(error.message);
+            }
         }
-    // then(r => r) untuk membersihkan
-    getUsers().then((r) => r)
+        // then(r => r) untuk membersihkan
+        getUsers().then((r) => r)
     }, [])
 
-    return <PlaceContentCenter></PlaceContentCenter>
+    return (
+        <PlaceContentCenter>
+            <Card>
+                <Card.Tittle>Users: {users.length}</Card.Tittle>
+                <Card.Body>
+                    {users.length ? (
+                        <ol>
+                            {users.map((user) => (
+                                <li key={user.id}>{user.name} ({user.username})</li>
+                            ))}
+                        </ol>
+                    ) : (
+                        <div> Tidak ada User</div>
+                    )}
+                </Card.Body>
+            </Card>
+        </PlaceContentCenter>
+    )
 }
